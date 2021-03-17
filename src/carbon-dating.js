deaea0c3a65488f30ad3932dee0ed0b7543ed8cc
OKitel <kitelolga@gmail.com>
2021-03-17 02:22:33 +0100

    refactor: clean code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,19 +4,22 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) { 
-  if (typeof(sampleActivity) == 'string') {
-    if (sampleActivity > 0 && sampleActivity < 1000) { 
-      if (!!sampleActivity) {
-        sampleActivity = +sampleActivity;
-        if (Number.isFinite(sampleActivity) == true) {
-          let k = 0.693 / HALF_LIFE_PERIOD;
-          let logN = Math.log(MODERN_ACTIVITY/sampleActivity);
-          let age = logN/k;
-          if (age < 0) {
-            return false
-          } else return Math.ceil(age);
-        } else return false;
-      }  else return false;
-    } else return false;
-  } else return false;
-};
+  if (typeof(sampleActivity) !== 'string') {
+    return false;
+  }
+  
+  if (sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+
+  sampleActivity = +sampleActivity;
+
+  if (sampleActivity === 0) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const logN = Math.log(MODERN_ACTIVITY/sampleActivity);
+  const age = logN/k;
+  return Math.ceil(age);
+}
