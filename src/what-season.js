f447ce79f83d07544414e4d855336e2034d60679
OKitel <kitelolga@gmail.com>
2021-03-20 00:17:53 +0100

    refactor: clean code

--- src/what-season.js
+++ src/what-season.js
@@ -5,17 +5,17 @@ module.exports = function getSeason(date) {
     return 'Unable to determine the time of year!';
   }
   let month = date.getMonth();
-  let day = date.getDate();
-  if ((day >= 1 && month == 11) || (day <= 28 && month <= 1)) {
+  
+  if ((month == 11) || (month <= 1)) {
     return 'winter';
   }
-  if ((day >= 1 && month == 2) || (day <= 31 && month <= 4)) {
+  if (month <= 4) {
     return 'spring';
   }
-  if ((day >= 1 && month == 5) || (day <= 31 && month <= 7)) {
+  if (month <= 7) {
     return 'summer';
   }
-  if ((day >= 1 && month == 8) || (day <= 30 && month <= 10)) {
+  if (month <= 10) {
     return 'autumn';
   }
 };
