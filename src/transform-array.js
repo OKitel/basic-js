a5b496e253ff7d7c0fcc99ddf064ba7e43bf20a6
OKitel <kitelolga@gmail.com>
2021-03-29 01:36:14 +0200

    fix: solve Error tests in transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
   let array = arr.slice();
 
   return array;
