c479bc3a13818bdd3a05c72f3828ef166d23434a
OKitel <kitelolga@gmail.com>
2021-03-29 16:57:13 +0200

    fix: advanced test in transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,32 +5,48 @@ module.exports = function transform(arr) {
     throw new Error();
   }
   let array = arr.slice();
+  let discarded = false;
+  let doubled = false;
   for (let i = 0; i < array.length; i++) {
-    switch (arr[i]) {
+    switch (array[i]) {
       case '--discard-next':
-        if (i == array.length - 1) {
+        if (i == array.length - 1 || discarded == true) {
           array.splice(i,1);
           break;
         }
         array.splice(i,2);
+        discarded = true;
+		i = i - 1;
+        break;
       case '--discard-prev':
-        if (i == 0) {
+        if (i == 0 || discarded == true) {
           array.splice(i,1);
           break;
         }
         array.splice(i-1,2);
+        discarded = true;
+		i = i - 1;
+        break;
       case '--double-next':
         if (i == array.length - 1) {
           array.splice(i,1);
           break;
         }
-        array.splice(i,1,i+1);
+        array.splice(i,1,array[i+1]);
+        doubled = true;
+        break;
       case '--double-prev':
         if (i == 0) {
           array.splice(i,1);
           break;
         }
-        array.splice(i,1,i-1);
+		  if (discarded == true) {
+			array.splice(i,1);
+			break;
+		  }
+      array.splice(i,1,array[i-1]);
+       break;
+        
     }
   }
 
