a17dd799e1c7f94f3435226e2cdb321882f16a61
OKitel <kitelolga@gmail.com>
2021-03-30 16:06:12 +0200

    fix: all tests in transform-array pass

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error();
@@ -9,7 +7,7 @@ module.exports = function transform(arr) {
   for (let i = 0; i < array.length; i++) {
     switch (array[i]) {
       case "--discard-next":
-        if (i == array.length - 1 || discarded == true) {
+        if (i == array.length - 1) {
           array.splice(i, 1);
           break;
         }
@@ -20,11 +18,12 @@ module.exports = function transform(arr) {
       case "--discard-prev":
         if (i == 0 || discarded == true) {
           array.splice(i, 1);
+          i = i - 1;
           break;
         }
         array.splice(i - 1, 2);
-        discarded = true;
-        i = i - 1;
+        //discarded = true;
+        i = i - 2;
         break;
       case "--double-next":
         if (i == array.length - 1) {
@@ -36,14 +35,18 @@ module.exports = function transform(arr) {
       case "--double-prev":
         if (i == 0) {
           array.splice(i, 1);
+          i = i - 1;
           break;
         }
         if (discarded == true) {
           array.splice(i, 1);
+          i = i - 1;
           break;
         }
         array.splice(i, 1, array[i - 1]);
         break;
+      default:
+        discarded = false;
     }
   }
 
