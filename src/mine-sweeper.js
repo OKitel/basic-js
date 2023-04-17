bc0caa87a46635f494f256d25d472fd296299ea7
OKitel <kitelolga@gmail.com>
2023-04-17 13:59:43 +0200

    feat: add solution for minesweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,11 +21,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const arr = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let sum = 0;
+      if (i === 0) {
+        if (matrix[i][j + 1]) {
+          sum++;
+        }
+        if (matrix[i][j - 1]) {
+          sum++;
+        }
+        if (matrix[i + 1][j]) {
+          sum++;
+        }
+        if (matrix[i + 1][j + 1]) {
+          sum++;
+        }
+        if (matrix[i + 1][j - 1]) {
+          sum++;
+        }
+      } else if (i === matrix.length - 1) {
+        if (matrix[i][j + 1]) {
+          sum++;
+        }
+        if (matrix[i][j - 1]) {
+          sum++;
+        }
+        if (matrix[i - 1][j]) {
+          sum++;
+        }
+        if (matrix[i - 1][j + 1]) {
+          sum++;
+        }
+        if (matrix[i - 1][j - 1]) {
+          sum++;
+        }
+      } else {
+        if (matrix[i][j - 1]) {
+          sum++;
+        }
+        if (matrix[i][j + 1]) {
+          sum++;
+        }
+        if (matrix[i - 1][j]) {
+          sum++;
+        }
+        if (matrix[i - 1][j + 1]) {
+          sum++;
+        }
+        if (matrix[i - 1][j - 1]) {
+          sum++;
+        }
+        if (matrix[i + 1][j]) {
+          sum++;
+        }
+        if (matrix[i + 1][j + 1]) {
+          sum++;
+        }
+        if (matrix[i + 1][j - 1]) {
+          sum++;
+        }
+      }
+      arr.push(sum);
+      if (j === matrix[i].length - 1) {
+        result.push(arr);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
